ac0e04ce5af78740b695f9015fc90083916f92fe
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-05-24 01:38:56 +0630

    Correct linter errors for script file

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const hamburger = document.querySelector(".hamburger");
-const navMobile = document.querySelector(".mobile-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMobile = document.querySelector('.mobile-menu');
 
-hamburger.addEventListener("click", toggleMenu);
+function toggleMenu() {
+  hamburger.classList.toggle('active');
+  navMobile.classList.toggle('active');
+}
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMobile.classList.remove('active');
+}
 
-document.querySelectorAll(".nav-link").forEach((n) => {
-  n.addEventListener("click", () => {
+hamburger.addEventListener('click', toggleMenu);
+
+document.querySelectorAll('.nav-link').forEach((n) => {
+  n.addEventListener('click', () => {
     closeMenu();
   });
 });
-
-function toggleMenu() {
-  hamburger.classList.toggle("active");
-  navMobile.classList.toggle("active");
-}
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMobile.classList.remove("active");
-}
