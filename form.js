69c176869cce8c65a5f42c38563dc008044fc642
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-05-24 16:41:28 +0200

    Added form linked form varibles

---
+++ form.js
@@ -0,0 +1,4 @@
+const contactForm = document.getElementById("form");
+const email = document.getElementById("email");
+const submit = document.getElementById("btn");
+const errorMessage = document.getElementById("error-msg");
