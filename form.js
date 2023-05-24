686e4ac85fe7e754f61bdc15a22ac54e9cac7462
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-05-24 21:14:00 +0630

    Add function for email error message

--- form.js
+++ form.js
@@ -2,3 +2,13 @@ const contactForm = document.getElementById("form");
 const email = document.getElementById("email");
 const submit = document.getElementById("btn");
 const errorMessage = document.getElementById("error-msg");
+
+form.addEventListener("submit", function(event) {
+    if (email.value === email.value.toLowerCase()) {
+      return true;
+    } else {
+      errorMessage.style.display = "block";
+      event.preventDefault();
+      return false;
+    }
+  });
